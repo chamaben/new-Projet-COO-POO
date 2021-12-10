2d67f4e40b04518b392e051ac4a00a30ab8b564d
Carinou <carinamalleville@gmail.com>
2021-12-10 12:00:55 +0100

    Création TCP_client

---
+++ projetfx/src/main/java/projetfx/projetfx/TCP_client.java
@@ -0,0 +1,13 @@
+package projetfx.projetfx;
+
+public class TCP_client {
+	
+	//le destinataire reçoit la requête TCP, il l'accepte et lance un thread puis revient sur accept (boucle infinie)
+	//initialiser la connexion TCP
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
