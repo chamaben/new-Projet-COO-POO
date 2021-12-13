34ed5dc67e6710a5db699d729dea336927613436
Clémence <clemence.lemeilleur@icloud.com>
2021-12-13 15:05:51 +0100

    Avancée de TCP Serveur

--- projetfx/src/main/java/projetfx/projetfx/TCP_serveur.java
+++ projetfx/src/main/java/projetfx/projetfx/TCP_serveur.java
@@ -2,6 +2,8 @@ package projetfx.projetfx;
 
 import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
 
 
 
@@ -9,11 +11,26 @@ public class TCP_serveur
 {
 	
 	// le serveur est en état accept, quand il reçoit la requête, il accepte la connexion, lance un thread qui va envoyer le messages, et rebouble sur accept
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) throws IOException {		
+		
 		//création du socket
-		ServerSocket servSocket = new ServerSocket(7878);
-	
+		ServerSocket s = new ServerSocket(7878);
+	    System.out.println(s.getLocalPort());
+	    
+	    while(true) {
+	    	//Mise en attente du serveur
+	      Socket serviceSocket =  s.accept();
+	      
+	    //Mise en place des échange de données en entrée et sortie
+	      PrintStream output = new PrintStream(serviceSocket.getOutputStream());
+	      
+	   // Envoyer et recevoir de la donnée
+	      output.println(new Date());
+	      
+	    //Fermer la connection
+	      serviceSocket.close();
+	    }
+	    
 	}
 	
 		
