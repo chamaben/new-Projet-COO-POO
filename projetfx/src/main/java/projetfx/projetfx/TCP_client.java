93aa1d51632c8eda145dddd09623fad9e593059d
MaCarina <carina.ma@live.fr>
2021-12-13 16:32:21 +0100

    Essai TCP_client, fonctionne en localhost

--- projetfx/src/main/java/projetfx/projetfx/TCP_client.java
+++ projetfx/src/main/java/projetfx/projetfx/TCP_client.java
@@ -1,6 +1,6 @@
 package projetfx.projetfx;
-
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -11,9 +11,85 @@ public class TCP_client {
 	
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		// TODO Auto-generated method stub
-		Socket link = new Socket("ad ip user",7878);
-		//new BufferedReader(new InputStreamReader(link.getInputStream()));
-		PrintWriter out = new PrintWriter(link.getOutputStream(),true);
-		link.close();
+		BufferedWriter os = null;
+	    BufferedReader is = null;
+	    String[] words = null;
+		System.out.println("je suis ici1");
+		
+		//lire l'ad IP dans le fichier
+	    try
+	    {
+	      // Le fichier d'entrée
+	      File file = new File("/home/mallevil/Documents/POO/projetfx/src/main/java/projetfx/projetfx/liste.txt");    
+	      // Créer l'objet File Reader
+	      FileReader fr = new FileReader(file);  
+	      // Créer l'objet BufferedReader        
+	      BufferedReader br = new BufferedReader(fr);  
+	      StringBuffer sb = new StringBuffer();    
+	      String line;
+	      while((line = br.readLine()) != null)
+	      {
+	        // ajoute la ligne au buffer
+	        sb.append(line);      
+	        sb.append("\n");   
+	        words = line.split(",");
+		    //System.out.println(words[1]);  
+	      }
+	      fr.close();    
+	      //System.out.println("Contenu du fichier: ");
+	      //System.out.println(sb.toString());  
+	    }
+	    catch(IOException e)
+	    {
+	      e.printStackTrace();
+	    }
+	    
+	    System.out.println("avant création socket");
+		Socket socketOfClient = new Socket("localhost",Integer.parseInt(words[2]));
+		System.out.println("sock créé");
+		// Create output stream at the client (to send data to the server)
+        os = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
+
+        // Input stream at Client (Receive data from the server).
+        is = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
+        
+        try {
+
+            // Write data to the output stream of the Client Socket.
+            os.write("HELO");
+  
+            // End of line
+            os.newLine();
+    
+            // Flush data.
+            os.flush();  
+            os.write("I am Tom Cat");
+            os.newLine();
+            os.flush();
+            os.write("QUIT");
+            os.newLine();
+            os.flush();
+
+
+            
+            // Read data sent from the server.
+            // By reading the input stream of the Client Socket.
+            String responseLine = is.readLine();
+            /*while ((responseLine = is.readLine()) != null) {
+                System.out.println("Server: " + responseLine);
+                if (responseLine.indexOf("OK") != -1) {
+                    break;
+                }
+            }*/
+            System.out.println("S");
+            os.close();
+            is.close();
+            socketOfClient.close();
+        } catch (UnknownHostException e) {
+            System.err.println("Trying to connect to unknown host: " + e);
+        } catch (IOException e) {
+            System.err.println("IOException:  " + e);
+        }
+		System.out.println("je suis ici");
 	}
 }
--- projetfx/src/main/java/projetfx/projetfx/TCP_serveur.java
+++ projetfx/src/main/java/projetfx/projetfx/TCP_serveur.java
@@ -13,22 +13,26 @@ public class TCP_serveur
 	// le serveur est en état accept, quand il reçoit la requête, il accepte la connexion, lance un thread qui va envoyer le messages, et rebouble sur accept
 	public static void main(String[] args) throws IOException {		
 		
-		//création du socket
-		ServerSocket s = new ServerSocket(7878);
-	    System.out.println(s.getLocalPort());
-	    
-	    while(true) {
-	    	//Mise en attente du serveur
-	      Socket serviceSocket =  s.accept();
-	      
-	    //Mise en place des échange de données en entrée et sortie
-	      PrintStream output = new PrintStream(serviceSocket.getOutputStream());
-	      
-	   // Envoyer et recevoir de la donnée
-	      output.println(new Date());
-	      
-	    //Fermer la connection
-	      serviceSocket.close();
+		try {
+			//création du socket
+			ServerSocket s = new ServerSocket(7891);
+		    System.out.println(s.getLocalPort());
+		    
+		    //while(true) {
+		    	//Mise en attente du serveur
+		      Socket serviceSocket =  s.accept();
+		      
+		    //Mise en place des échange de données en entrée et sortie
+		      PrintStream output = new PrintStream(serviceSocket.getOutputStream());
+		      
+		   // Envoyer et recevoir de la donnée
+		      output.println(new Date());
+		      
+		    //Fermer la connection
+		      serviceSocket.close();
+		    //}
+		} catch (IOException e) {
+	        e.printStackTrace();
 	    }
 	    
 	}
