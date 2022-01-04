b93050ae4bf1265aa7297fc65035f774db2f020e
chamaben <chamabenlafkih.cb@gmail.com>
2022-01-04 08:59:25 +0100

    merge

--- projetfx/src/main/java/projetfx/projetfx/Message.java
+++ projetfx/src/main/java/projetfx/projetfx/Message.java
@@ -1,5 +1,6 @@
 package projetfx.projetfx;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Message {
@@ -10,9 +11,9 @@ public class Message {
 	//public User recepteur;
 	public String recepteur;
 	public String contenu;
-	public Date date;
+	public LocalDateTime date;
 	
-	public Message(String emetteur1, String recepteur1, String contenu1, Date date2) {
+	public Message(String emetteur1, String recepteur1, String contenu1, LocalDateTime date2) {
 		this.emetteur= emetteur1;
 		this.recepteur= recepteur1;
 		this.contenu= contenu1;
