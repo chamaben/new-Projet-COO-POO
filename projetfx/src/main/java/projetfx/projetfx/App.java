0880451bb7f116f96fa1339a22a27f464617bec0
chamaben <chamabenlafkih.cb@gmail.com>
2022-01-06 16:05:23 +0100

    deconnexion ok

--- projetfx/src/main/java/projetfx/projetfx/App.java
+++ projetfx/src/main/java/projetfx/projetfx/App.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * JavaFX App
@@ -26,6 +28,14 @@ public class App extends Application {
         stage.setScene(scene);
         stage.setTitle("Chat");
         stage.show();
+        stage.setOnCloseRequest(event -> {
+        	try {
+				deconnexion();
+			} catch (ClassNotFoundException | SQLException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        });
     }
    
     static void setRoot(String fxml) throws IOException {
@@ -39,6 +49,20 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    private void deconnexion() throws ClassNotFoundException, SQLException, IOException 
+    {
+		WindowModel.user.etat=0;
+		DbConnect.Connexion();
+		Statement stmt = DbConnect.connection.createStatement();
+		String query = "UPDATE user SET etat='0' WHERE login='"+WindowModel.user.login+"'";
+		stmt.executeUpdate(query);
+		DbConnect.FinConnexion();
+		// supprime le user de la liste active members
+		@SuppressWarnings({ "unused", "unlikely-arg-type" })
+		boolean rem = WindowModel.activeMembers.remove(WindowModel.user);
+			
     }
 
 }
