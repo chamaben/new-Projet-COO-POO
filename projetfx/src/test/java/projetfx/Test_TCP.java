c22b94473994deaefd58e66c0f56cfce22c87d75
Carinou <carinamalleville@gmail.com>
2022-01-20 17:14:51 +0100

    Update test tcp

--- projetfx/src/test/java/projetfx/Test_TCP.java
+++ projetfx/src/test/java/projetfx/Test_TCP.java
@@ -1,5 +1,14 @@
 package projetfx;
 
-public class Test_TCP {
+import org.junit.Test;
+import java.sql.SQLException;
+import projetfx.projetfx.*;
 
+public class Test_TCP {
+	
+	@Test
+    public void testTCP() throws ClassNotFoundException, SQLException {
+		TCP_serveur.receive();
+		TCP_client.send("hey", "hey", "hey");
+	}
 }
