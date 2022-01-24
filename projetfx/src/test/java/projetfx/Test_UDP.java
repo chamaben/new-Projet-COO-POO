fabbd9777858895c681bc3fa5255bfe1db521d61
Carinou <carinamalleville@gmail.com>
2022-01-24 08:38:22 +0100

    Création test UDP

---
+++ projetfx/src/test/java/projetfx/Test_UDP.java
@@ -0,0 +1,16 @@
+package projetfx;
+
+import projetfx.projetfx.*;
+import java.io.IOException;
+import java.sql.SQLException;
+import org.junit.Test;
+
+class Test_UDP {
+
+	@Test
+	void testUDP() throws ClassNotFoundException, IOException, SQLException {
+		UDP_serveur.run();
+		UDP_client.connexion("chama", "1", "100.00.00.00");
+	}
+
+}
