2dd751bfcd2a5662cde3b3e2da486d97f6d1e67c
MaCarina <carina.ma@live.fr>
2022-01-26 16:52:53 +0100

    ajout classe varGlobal pour config

---
+++ projetfx/src/main/java/projetfx/projetfx/VarGlobal.java
@@ -0,0 +1,8 @@
+package projetfx.projetfx;
+
+public class VarGlobal {
+	public static String adBroad = "10.1.255.255";
+	public static int portBroad = 7907;
+	public static int portTCP = 7899;
+	public static String Interface = "eth0";
+}
